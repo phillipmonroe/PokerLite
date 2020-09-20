1ad68fbf7518f295bcbea5b9230177725fa3d00f
Phillip Monroe <phillipmonroe@u.boisestate.edu>
2020-09-19 18:38:25 -0600

    added deck for easier testing

---
+++ Deck.cpp
@@ -0,0 +1,39 @@
+Card aceSpades(A, Card::Spades);
+Card twoSpades(2, Card::Spades);
+Card threeSpades(3, Card::Spades);
+Card fourSpades(4, Card::Spades);
+Card fiveSpades(5, Card::Spades);
+Card sixSpades(6, Card::Spades);
+Card sevenSpades(7, Card::Spades);
+Card eightSpades(8, Card::Spades);
+Card nineSpades(9, Card::Spades);
+
+Card aceHearts(A, Card::Hearts);
+Card twoHearts(2, Card::Hearts);
+Card threeHearts(3, Card::Hearts);
+Card fourHearts(4, Card::Hearts);
+Card fiveHearts(5, Card::Hearts);
+Card sixHearts(6, Card::Hearts);
+Card sevenHearts(7, Card::Hearts);
+Card eightHearts(8, Card::Hearts);
+Card nineHearts(9, Card::Hearts);
+
+Card aceClubs(A, Card::Clubs);
+Card twoClubs(2, Card::Clubs);
+Card threeClubs(3, Card::Clubs);
+Card fourClubs(4, Card::Clubs);
+Card fiveClubs(5, Card::Clubs);
+Card sixClubs(6, Card::Clubs);
+Card sevenClubs(7, Card::Clubs);
+Card eightClubs(8, Card::Clubs);
+Card nineClubs(9, Card::Clubs);
+
+Card aceDiamonds(A, Card::Diamonds);
+Card twoDiamonds(2, Card::Diamonds);
+Card threeDiamonds(3, Card::Diamonds);
+Card fourDiamonds(4, Card::Diamonds);
+Card fiveDiamonds(5, Card::Diamonds);
+Card sixDiamonds(6, Card::Diamonds);
+Card sevenDiamonds(7, Card::Diamonds);
+Card eightDiamonds(8, Card::Diamonds);
+Card nineDiamonds(9, Card::Diamonds);
